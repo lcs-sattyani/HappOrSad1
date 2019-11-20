25fc9f4de4137b9113d053a9932721f9120a68a4
Sawsan Attyani <sattyani@lcs.on.ca>
2019-11-20 11:15:54 -0500

    Initial Commit

---
+++ HappOrSad1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappOrSad1
+//
+//  Created by Attyani, Sawsan on 2019-11-20.
+//  Copyright © 2019 Attyani, Sawsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
